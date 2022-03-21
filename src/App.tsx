27a0a5bda8b9ed0a851c07b496c9d214161b3e32
Ryan Long <rylong18@gmail.com>
2022-03-21 09:45:39 -0400

    update dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { Fragment } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 import Landing from './routes/Landing';
 import NotFound from './routes/NotFound';
@@ -10,10 +10,10 @@ const App: React.FC = () => {
 
   return (
     <Fragment>
-      <Switch>
-        <Route exact path='/' component={ Landing }/>
-        <Route component={ NotFound } />
-      </Switch>
+      <Routes>
+        <Route path='/' element={ <Landing/> }/>
+        <Route element={ <NotFound/> } />
+      </Routes>
     </Fragment>
   );
 };
--- src/components/section/Section.tsx
+++ src/components/section/Section.tsx
@@ -5,25 +5,19 @@ import './Section.scss';
 
 interface MainProps {
   className?: string,
-  theme?: 'dark' | 'light',
   children: ReactNode,
 }
 
-const Section: React.FC<MainProps> = ({ className, theme, children, ...props }) => {
+const Section: React.FC<MainProps> = ({ className, children, ...props }) => {
 
   const classes = classNames(
     'section',
-    `section__theme-${theme}`,
     className
   );
 
   return (
     <section className={classes} {...props}>{children}</section>
   );
-};
-
-Section.defaultProps = {
-  theme: 'light'
 };
 
 export default Section;
